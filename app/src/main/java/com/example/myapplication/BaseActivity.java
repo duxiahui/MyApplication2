69df13fa8eb208febec7d9f5084cc2f25968f466
dxh1234567 <duxiahui0805@163.com>
2019-07-13 09:51:32 +0800

    extents aCTIVITY

--- app/src/main/java/com/example/myapplication/BaseActivity.java
+++ app/src/main/java/com/example/myapplication/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 /**
  * Created by du.xiahui
  */
-public class BaseActivity {
+public class BaseActivity  extends AppCompatActivity {
+
+
 }
